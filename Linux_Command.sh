7f2a75bbe52b1571c38aeebed0520367df816099
quy-d0 <78673384+quy-d0@users.noreply.github.com>
2022-03-30 16:58:51 +1100

    Create Linux_Command.sh

---
+++ Linux_Command.sh
@@ -0,0 +1,15 @@
+#Shell can be seen as a command interpreter and a programming language (C Shell - csh, Bash Shell - bash)
+#The command handles the hardware via the kernel
+#Typical commands: ls, mv, mkdir, pwd
+#Start with #!/bin/bash 
+
+#!/bin/bash 
+
+#Make the bash file executable 
+chmod u+x filename.sh
+
+#To run the script:
+./con-sh-exe.sh
+
+#Echo - command prints out text/value of variables
+
